0702b0b6c6886dec09b7c73c36461e12742811ae
nfmohit-wpmudev <nfmohit49@gmail.com>
2019-09-16 17:30:20 +0600

    Introduced support for BrowserSync

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const path = require( 'path' );
-var ExtractTextPlugin = require( 'extract-text-webpack-plugin' );
+const ExtractTextPlugin = require( 'extract-text-webpack-plugin' );
+const BrowserSyncPlugin = require( 'browser-sync-webpack-plugin' );
 
 module.exports = {
 	entry: './assets/src/js/main.js',
@@ -11,35 +12,40 @@ module.exports = {
 	module: {
 		rules: [
 			{
-				test:/\.(s*)css$/,
-				use: ExtractTextPlugin.extract( {
-					fallback:'style-loader',
-                    use:[ 'css-loader', 'resolve-url-loader', 'sass-loader' ],
-                } )
+				test: /\.(s*)css$/,
+				use: ExtractTextPlugin.extract({
+					fallback: 'style-loader',
+					use: [ 'css-loader', 'resolve-url-loader', 'sass-loader' ],
+				})
 			},
 			{
-		    test: /\.(woff2?|ttf|otf|eot|svg)$/,
-			    loader: 'file-loader',
-			    options: {
-			        name: '[name].[ext]',
-			        outputPath: '../fonts/'
-			    }
+				test: /\.(woff2?|ttf|otf|eot|svg)$/,
+				loader: 'file-loader',
+				options: {
+					name: '[name].[ext]',
+					outputPath: '../fonts/'
+				}
 			},
 			{
-		    test: /\.(jpg|png|jpeg)$/,
-			    exclude: /node_modules/,
-			    loader: 'file-loader',
-			    options: {
-			        name: '[name].[ext]',
-			        outputPath: '../../images/'
-			    }
+				test: /\.(jpg|png|jpeg)$/,
+				exclude: /node_modules/,
+				loader: 'file-loader',
+				options: {
+					name: '[name].[ext]',
+					outputPath: '../../images/'
+				}
 			}
 		]
 	},
 
 	plugins: [
-		new ExtractTextPlugin('../css/main.min.css', {
-            allChunks: true
-        })
+		new BrowserSyncPlugin( {
+			host: 'localhost',
+			port: 3000,
+			server: { baseDir: [ './' ] }
+		} ),
+		new ExtractTextPlugin( '../css/main.min.css', {
+			allChunks: true
+		} )
 	]
 };
